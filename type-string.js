d64ab225ab5e94b1652bd9c5879ed7c216479c6c
Leo <leonhardocueto@gmail.com>
2023-12-09 00:08:10 -0300

    first commit

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá mundo";
+const texto2 = 'Olá mundo';
+const senha = "senhaSuperSegurs456!";
+const StringDeNumeros = "34567";
+
+const citacao = "O meu disse nome é ";
+const meuNome = "Leonardo";
+
+// concsatenação (+)
+
+console.log(citacao + meuNome);
+
+// templete string OU templete literal
+
