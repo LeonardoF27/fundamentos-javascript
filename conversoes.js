15147f137e9ada09f6599a736fd03e197019d60d
Leo <leonhardocueto@gmail.com>
2023-12-11 10:16:37 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroSting = Number("456a");
+
+// Number()
+// String()
+console.log(numero + numeroSting);
+
+// conversãoes explícita
+
