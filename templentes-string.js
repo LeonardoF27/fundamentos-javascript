f279ce093d4ab29009575beeda796623d4a0e615
Leo <leonhardocueto@gmail.com>
2023-12-21 12:12:37 -0300

    operadores e funções

---
+++ templentes-string.js
@@ -0,0 +1,9 @@
+const nome = "Leo";
+const idade = 2023-2001;
+const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome ${nome} , minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
